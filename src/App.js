b1b084b0474a6eb4a5e36ecbbd8a3312bc0876d2
saurav07-jain <sauravjain0711@gmail.com>
2021-12-13 07:01:02 +0530

    App

--- src/App.js
+++ src/App.js
@@ -43,20 +43,20 @@ function App() {
     <>
       {/* <Router> */}
 
-        <Navbar mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3" >
-      <TextForm showAlert={showAlert} heading="Enter Your Text" mode={mode} /> 
-          {/* <Routes>
+      <Navbar mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3" >
+        <TextForm showAlert={showAlert} heading="Enter Your Text" mode={mode} />
+        {/* <Routes>
             <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter Your Text" mode={mode} />}>
             </Route>
             <Route exact path="About" element={<About />}>
             </Route>
           </Routes> */}
-        </div>
+      </div>
       {/* </Router> */}
 
-       
+
     </>
   );
 
